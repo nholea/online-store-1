5ad2baf7d5596d3faadb966b4714395f3b031587
Julieta Martin <julieta.martin@clarity.ai>
2022-09-18 21:26:57 +0200

    new product added into ProductWarehouse class

--- app/src/main/java/gradle/java/ProductWarehouse.java
+++ app/src/main/java/gradle/java/ProductWarehouse.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 
 public class ProductWarehouse {
 
-    private final ArrayList<Product> products = Arrays.asList(new Product(1), new Product(2), new Product(3));
+    private final ArrayList<Product> products = (ArrayList<Product>) Arrays.asList(new Product(
+            "\uD83D\uDCFA",
+            "Glamorous panoramic television, 13 inches",
+            "With this panoramic television, your friday nights will be boring no more.",
+            300.99,
+            "W2C"));
 
     public ArrayList<Product> findAll() {
         return products;
