b72dd84f469af25a750d642d7bfeafd95373e5b9
Julieta Martin <julieta.martin@clarity.ai>
2022-09-18 21:24:14 +0200

    catalogue formater created

--- app/src/main/java/gradle/java/CatalogueFormater.java
+++ app/src/main/java/gradle/java/CatalogueFormater.java
@@ -4,7 +4,18 @@ import java.util.ArrayList;
 
 public class CatalogueFormater {
 
+    private ProductWarehouse productWarehouse = new ProductWarehouse();
+    private Product product;
+
     public String format(ArrayList<Product> catalogue){
-        return "";
-    }
+            for (int i = 0; i < catalogue.size(); i++){
+                System.out.println(product.showImage());
+                System.out.println(product.showDescription());
+                System.out.println(product.showFeaturedAttribute());
+                System.out.println(product.showPrice());
+                System.out.println(product.showReference());
+            }
+
+            return "";
+        }
 }
