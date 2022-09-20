3a7c09d42399ec860560f2821bde82e9788454fe
Julieta Martin <julieta.martin@clarity.ai>
2022-09-20 22:58:48 +0200

    feature: CatalogueFormater implemented with format method

--- app/src/main/java/gradle/java/CatalogueFormater.java
+++ app/src/main/java/gradle/java/CatalogueFormater.java
@@ -1,21 +1,18 @@
 package gradle.java;
 
+
 import java.util.ArrayList;
 
 public class CatalogueFormater {
-
-    private ProductWarehouse productWarehouse = new ProductWarehouse();
-    private Product product;
-
     public String format(ArrayList<Product> catalogue){
-            for (int i = 0; i < catalogue.size(); i++){
+            for (Product product : catalogue){
                 System.out.println(product.showImage());
                 System.out.println(product.showDescription());
                 System.out.println(product.showFeaturedAttribute());
                 System.out.println(product.showPrice());
                 System.out.println(product.showReference());
+                System.out.println("\n");
             }
-
-            return "";
+        return "";
         }
 }
