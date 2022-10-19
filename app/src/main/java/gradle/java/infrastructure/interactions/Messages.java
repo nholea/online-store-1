ec0959bbb9cfb5db19b0cc28e6364b0cd56ee61a
Natalia <natalia.hernandez@clarity.ai>
2022-10-19 20:01:14 +0200

    refactor: added Magic Strings to Messages class

---
+++ app/src/main/java/gradle/java/infrastructure/interactions/Messages.java
@@ -0,0 +1,16 @@
+package gradle.java.infrastructure.interactions;
+
+public class Messages {
+
+  public static final String PRODUCT_ELECTION = "Which product would you like to explore? - Introduce a product's reference.";
+
+  public static final String NEXT_STEP = "¿What would you like to do next?";
+
+  public static final String ADD_PRODUCT = "1 - Add product to cart";
+
+  public static final String SHOW_PRODUCTS = "2 - Keep browsing products";
+
+  public static final String INVALID_OPTION = "Sorry, you have to choose one valid option";
+
+
+}
--- app/src/main/java/gradle/java/infrastructure/interactions/Options.java
+++
@@ -1,8 +0,0 @@
-package gradle.java.infrastructure.interactions;
-
-public class Options {
-
-  public static final String PRODUCT_ELECTION = "Which product would you like to explore? - Introduce a product's reference.";
-
-
-}
