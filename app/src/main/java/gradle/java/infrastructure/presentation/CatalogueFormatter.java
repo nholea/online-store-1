ba1087c317d191aac7a3d54d290e67b66f4d4ed3
Natalia <natalia.hernandez@clarity.ai>
2022-10-19 18:24:21 +0200

    feat: implements findByReference method

--- app/src/main/java/gradle/java/infrastructure/presentation/CatalogueFormatter.java
+++ app/src/main/java/gradle/java/infrastructure/presentation/CatalogueFormatter.java
@@ -7,17 +7,63 @@ import java.util.ArrayList;
 public class CatalogueFormatter {
 
   public String outputFormat(ArrayList<Product> catalogue) {
-
     StringBuilder productFormat = new StringBuilder();
     for (Product product : catalogue) {
-      productFormat.append(product.image);
-      productFormat.append(product.description);
-      productFormat.append(product.featuredAttribute);
-      productFormat.append(product.price);
-      productFormat.append(product.reference);
-      productFormat.append("\n");
+      productAspectsFormat(productFormat, product.image);
+
+      productAspectsFormat(productFormat, product.shortDescription);
+
+      productAspectsFormat(productFormat, product.highlightedAttribute);
+
+      productAspectsFormat(productFormat, product.longDescription);
+
+      productDetailsFormat(productFormat, String.valueOf(product.price), "\uD83D\uDCB0 Price: %s €");
+
+      productDetailsFormat(productFormat, product.reference, "Reference: %s");
+      
     }
     return productFormat.toString();
+
+  }
+
+
+  public String outputProductDetailsFormat(Product product) {
+    StringBuilder productFormat = new StringBuilder();
+
+    productAspectsFormat(productFormat, product.image);
+
+    productDetailsFormat(productFormat, String.valueOf(product.price), "\uD83D\uDCB0 Price: %s €");
+
+    productDetailsFormat(productFormat, product.reference, "Reference: %s");
+
+    addExplanatoryTitle(productFormat, "SUMMARY:");
+
+    productAspectsFormat(productFormat, product.shortDescription);
+
+    productAspectsFormat(productFormat, product.highlightedAttribute);
+
+    addExplanatoryTitle(productFormat, "\nDESCRIPTION:");
+
+    productAspectsFormat(productFormat, product.longDescription);
+
+    return productFormat.toString();
+
+
+  }
+
+  private static void productAspectsFormat(StringBuilder productBuilder, String product) {
+    productBuilder.append(product);
+    productBuilder.append("\n");
+  }
+
+  private static void productDetailsFormat(StringBuilder productFormat, String product, String fieldProductFormat) {
+    productFormat.append(String.format(fieldProductFormat, product));
+    productFormat.append("\n");
+  }
+
+  private static void addExplanatoryTitle(StringBuilder productFormat, String title) {
+    productFormat.append(title);
+    productFormat.append("\n");
   }
 
 
--- app/src/main/java/gradle/java/infrastructure/repositories/impl/ProductWareHouse.java
+++ app/src/main/java/gradle/java/infrastructure/repositories/impl/ProductWareHouse.java
@@ -9,14 +9,16 @@ public class ProductWareHouse implements ProductRepository {
 
   private final ArrayList<Product> products = new ArrayList<>(Arrays.asList(new Product(
       "\uD83D\uDCFA",
-      "Glamorous panoramic television, 13 inches",
+      "Glamorous panoramic television",
       "With this panoramic television, your friday nights will be boring no more.",
+      ",13 inches",
       300.99,
       "W2C"),
     new Product(
       "\uD83C\uDFB9",
       "Untuned musical keyboard, 4 octaves",
       "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.",
+      ", 4 octaves",
       1003.00,
       "X4A")));
 
