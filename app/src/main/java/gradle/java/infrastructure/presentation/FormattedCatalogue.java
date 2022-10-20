02297b3a1842d880114c0e40d67c33722bd98900
Natalia <natalia.hernandez@clarity.ai>
2022-10-20 09:31:18 +0200

    feat: move outputFormattedProduct method into FormattedCatalogue class

--- app/src/main/java/gradle/java/infrastructure/presentation/FormattedCatalogue.java
+++ app/src/main/java/gradle/java/infrastructure/presentation/FormattedCatalogue.java
@@ -2,24 +2,30 @@ package gradle.java.infrastructure.presentation;
 
 import gradle.java.domain.Product;
 import gradle.java.domain.ProductRepository;
-
 import java.util.ArrayList;
 
 
 public class FormattedCatalogue {
 
-    private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-    private final CatalogueFormatter catalogueFormatter;
+  private final CatalogueFormatter catalogueFormatter;
 
-    public FormattedCatalogue(ProductRepository productRepository, CatalogueFormatter catalogueFormatter) {
-        this.productRepository = productRepository;
-        this.catalogueFormatter = catalogueFormatter;
-    }
+  public FormattedCatalogue(ProductRepository productRepository, CatalogueFormatter catalogueFormatter) {
+    this.productRepository = productRepository;
+    this.catalogueFormatter = catalogueFormatter;
+  }
 
-    public void outputFormattedCatalogue() {
-        ArrayList<Product> catalogue = productRepository.findAll();
-        String formattedCatalogue = catalogueFormatter.outputFormat(catalogue);
-        System.out.println(formattedCatalogue);
-    }
+  public void outputFormattedCatalogue() {
+    ArrayList<Product> catalogue = productRepository.findAll();
+    String formattedCatalogue = catalogueFormatter.outputFormat(catalogue);
+    System.out.println(formattedCatalogue);
+  }
+
+  public void outputFormattedProduct(String inputOption) {
+    Product chosenProduct = productRepository.findByReference(inputOption);
+    System.out.println(catalogueFormatter.outputProductDetailsFormat(chosenProduct));
+  }
+
+
 }
