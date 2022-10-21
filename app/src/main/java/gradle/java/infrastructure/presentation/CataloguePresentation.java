30ee8583ad4314a2227bb8eef947f4d702a0e562
Natalia <natalia.hernandez@clarity.ai>
2022-10-21 11:32:27 +0200

    feat: CataloguePresentation implements Presentation interface

--- app/src/main/java/gradle/java/infrastructure/presentation/CataloguePresentation.java
+++ app/src/main/java/gradle/java/infrastructure/presentation/CataloguePresentation.java
@@ -2,10 +2,11 @@ package gradle.java.infrastructure.presentation;
 
 import gradle.java.domain.Product;
 import gradle.java.domain.ProductRepository;
+import gradle.java.infrastructure.interactions.Presentation;
 import java.util.ArrayList;
 
 
-public class CataloguePresentation {
+public class CataloguePresentation implements Presentation {
 
   private final ProductRepository productRepository;
 
@@ -16,15 +17,14 @@ public class CataloguePresentation {
     this.catalogueFormatter = catalogueFormatter;
   }
 
-  public void outputFormattedCatalogue() {
-    ArrayList<Product> catalogue = productRepository.findAll();
+
+  public void outputFormattedProduct(Product chosenProduct) {
+    System.out.println(catalogueFormatter.productDetailsFormat(chosenProduct));
+  }
+
+  @Override
+  public void showProducts(ArrayList<Product> catalogue) {
     String formattedCatalogue = catalogueFormatter.productFormat(catalogue);
     System.out.println(formattedCatalogue);
   }
-
-  public void outputFormattedProduct(String inputOption) {
-    Product chosenProduct = productRepository.findByReference(inputOption);
-    System.out.println(catalogueFormatter.productDetailsFormat(chosenProduct));
-  }
-  
 }
