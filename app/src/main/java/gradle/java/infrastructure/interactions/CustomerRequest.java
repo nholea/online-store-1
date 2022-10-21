7230012cb03a857b1a359a43931a95af139c9fb2
Natalia <natalia.hernandez@clarity.ai>
2022-10-21 10:15:00 +0200

    feat: remove unused import

--- app/src/main/java/gradle/java/infrastructure/interactions/CustomerRequest.java
+++ app/src/main/java/gradle/java/infrastructure/interactions/CustomerRequest.java
@@ -1,49 +1,17 @@
 package gradle.java.infrastructure.interactions;
 
-import gradle.java.infrastructure.presentation.CataloguePresentation;
-import gradle.java.infrastructure.repositories.StringRepository;
 import java.util.Scanner;
 
 public class CustomerRequest {
 
-  private final CataloguePresentation cataloguePresentation;
+  public CustomerRequest() {
 
-  public CustomerRequest(CataloguePresentation cataloguePresentation) {
-    this.cataloguePresentation = cataloguePresentation;
   }
 
-  public String productsToChooseExplore() {
-    System.out.println(StringRepository.PRODUCT_ELECTION);
+  public String scanner() {
     Scanner scanner = new Scanner(System.in);
-    String inputOption = scanner.nextLine();
-    return inputOption;
+    return scanner.nextLine();
   }
 
 
-  public String decideWhatToDoNext() {
-    System.out.println("\n" + StringRepository.NEXT_STEP);
-    String nextOption1 = StringRepository.ADD_PRODUCT;
-    String nextOption2 = StringRepository.SHOW_PRODUCTS;
-    System.out.println(nextOption1);
-    System.out.println(nextOption2);
-
-    Scanner scanner = new Scanner(System.in);
-    String inputOption = scanner.nextLine();
-
-    return inputOption;
-  }
-
-  public void addToCartOrShowCatalogue(String inputOption) {
-    if (inputOption.equals(StringRepository.ONE)) {
-      System.out.println();
-    } else if (inputOption.equals(StringRepository.TWO)) {
-      cataloguePresentation.outputFormattedCatalogue();
-    } else {
-      System.out.println(StringRepository.INVALID_OPTION);
-    }
-  }
-
-  public void displayChosenProductByReference(String inputOption) {
-    cataloguePresentation.outputFormattedProduct(inputOption);
-  }
 }
