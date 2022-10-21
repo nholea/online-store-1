e3d05706ba3bfbeab7685de29950e23655e67738
Natalia <natalia.hernandez@clarity.ai>
2022-10-21 17:30:16 +0200

    feat: Create Catalogue resource class.

--- app/src/main/java/gradle/java/infrastructure/repositories/ProductWareHouse.java
+++ app/src/main/java/gradle/java/infrastructure/repositories/ProductWareHouse.java
@@ -11,14 +11,16 @@ public class ProductWareHouse implements ProductRepository {
   private final ArrayList<Product> products = new ArrayList<>(Arrays.asList(new Product(
       "\uD83D\uDCFA",
       "Glamorous panoramic television,",
-      "With this panoramic television, your friday nights will be boring no more.",
+      "With this panoramic television, your friday nights will be boring no more.\n"
+        + "The screen is composed of 14 million pixels.\n"
+        + "Thanks to its low energy consumption design, your bills will stay low.",
       " 13 inches",
       300.99,
       "W2C"),
     new Product(
       "\uD83C\uDFB9",
       "Untuned musical keyboard,",
-      "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour " +
+      "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour \n" +
         "will be ready to move to a building far away from you.",
       " 4 octaves",
       1003.00,
@@ -26,7 +28,9 @@ public class ProductWareHouse implements ProductRepository {
     new Product(
       "\uD83D\uDCFA",
       "Glamorous panoramic television,",
-      "With this panoramic television, your friday nights will be boring no more.",
+      "With this panoramic television, your friday nights will be boring no more.\n"
+        + "The screen is composed of 14 million pixels.\n"
+        + "Thanks to its low energy consumption design, your bills will stay low.",
       " 13 inches",
       300.99,
       "W2C")));
---
+++ app/src/test/java/gradle/java/CatalogueResource.java
@@ -0,0 +1,36 @@
+package gradle.java;
+
+import gradle.java.domain.Product;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CatalogueResource {
+
+  public static ArrayList<Product> products = new ArrayList<>(Arrays.asList(new Product(
+      "\uD83D\uDCFA",
+      "Glamorous panoramic television,",
+      "With this panoramic television, your friday nights will be boring no more.\n"
+        + "The screen is composed of 14 million pixels.\n"
+        + "Thanks to its low energy consumption design, your bills will stay low.",
+      " 13 inches",
+      300.99,
+      "W2C"),
+    new Product(
+      "\uD83C\uDFB9",
+      "Untuned musical keyboard,",
+      "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour \n" +
+        "will be ready to move to a building far away from you.",
+      " 4 octaves",
+      1003.00,
+      "X4A"),
+    new Product(
+      "\uD83D\uDCFA",
+      "Glamorous panoramic television,",
+      "With this panoramic television, your friday nights will be boring no more.\n"
+        + "The screen is composed of 14 million pixels.\n"
+        + "Thanks to its low energy consumption design, your bills will stay low.",
+      " 13 inches",
+      300.99,
+      "W2C")));
+
+}
