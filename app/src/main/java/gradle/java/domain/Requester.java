d0755f50236d0970150daa88153bbacaabe0346c
Natalia <natalia.hernandez@clarity.ai>
2022-11-05 16:59:07 +0100

    feat: create sendMessage method in Requester.java

--- app/src/main/java/gradle/java/domain/Requester.java
+++ app/src/main/java/gradle/java/domain/Requester.java
@@ -2,6 +2,8 @@ package gradle.java.domain;
 
 public interface Requester {
 
+  void sendMessage(String message);
+
   String demand();
 
 }
--- app/src/main/java/gradle/java/infrastructure/interactions/CustomerRequest.java
+++ app/src/main/java/gradle/java/infrastructure/interactions/CustomerRequest.java
@@ -4,7 +4,12 @@ import gradle.java.domain.Requester;
 import java.util.Scanner;
 
 public class CustomerRequest implements Requester {
-  
+
+  @Override
+  public void sendMessage(String message) {
+    System.out.println(message);
+  }
+
   @Override
   public String demand() {
     Scanner scanner = new Scanner(System.in);
