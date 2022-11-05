16f900b3594e201139e49de6ed9a9ba1e9de203f
Natalia <natalia.hernandez@clarity.ai>
2022-11-05 17:17:14 +0100

    feat: implements UserInterface method in HTTPUserInterface class

---
+++ app/src/main/java/gradle/java/infrastructure/interactions/HTTPUserInterface.java
@@ -0,0 +1,16 @@
+package gradle.java.infrastructure.interactions;
+
+import gradle.java.domain.UserInterface;
+
+public class HTTPUserInterface implements UserInterface {
+
+  @Override
+  public void sendMessage(String message) {
+    
+  }
+
+  @Override
+  public String demand() {
+    return null;
+  }
+}
